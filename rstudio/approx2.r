aebfc8075d99e1f697e4eb74a8dbebcce0bf10d4
AdrienGuichard <guichard.adrien@gmail.com>
2020-03-20 15:08:45 +0100

    better amplitude optimisation

---
+++ rstudio/approx2.r
@@ -0,0 +1,34 @@
+# d: dataset, m: mean, sd: width, a: aplitude
+
+normApplitude <- function(d, m, sd) {
+  d[length(d)] / pnorm(length(d):length(d), mean = m, sd = sd)
+}
+
+normVariance <- function(d, m, sd, a) {
+  var(d - pnorm(1:length(d), mean = m, sd = sd) * a)
+}
+
+mTab = c(length(d):(length(d)+40))
+sdTab = c(4:10)
+
+print(mTab)
+print(sdTab)
+vOrig = Inf
+
+aApprox = 0
+mApprox = 0
+sdApprox = 0
+
+for (a in c(5000:20000)) {
+  for (sd in sdTab) {
+    for(m in mTab) {
+      v = normVariance(italy, m, sd, a)
+      if (v < vOrig) {
+        sdApprox = sd
+        mApprox = m
+        aApprox = a
+        vOrig = v
+      }
+    }
+  }
+}
