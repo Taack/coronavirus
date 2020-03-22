125401f6233bdc9d6eb49e822778f7fc51b97d41
AdrienGuichard <guichard.adrien@gmail.com>
2020-03-22 07:04:43 +0100

    try to catch irregularity

---
+++ rstudio/amplitudeLoop.r
@@ -0,0 +1,13 @@
+deathApprox <- function(m, sd, a) {a*pnorm(1:length(data), mean = m, sd = sd)}
+
+results <- c(0)
+
+for (i in 11:length(data)) {
+  historyData <- data[1:i]
+  fitDn <- tryCatch({nlsLM(historyData~a*pnorm(1:i, mean = m, sd = sd), start=list(m=10,sd=2,a=100))},
+                    error = function(e) {print(e)})
+  print(as.list(coef(fitDn))$a)
+  results <- c(results, as.list(coef(fitDn))$a)
+}
+
+plot(results)
