2acc35b9da8ae5e1b13ff423858ea2d4b09f4a8c
AdrienGuichard <guichard.adrien@gmail.com>
2020-03-23 18:15:37 +0100

    updated all, add uk

---
+++ rstudio/uk.r
@@ -0,0 +1,9 @@
+# since March 05
+france = c(1,2,2,3,
+           5,6,8,10,11,21,35,
+           55,71,104,144,177,233,281,
+           335)
+
+population = 67000000
+data = france
+source('./approxnlsLM.r')
