652a0b50a835484d031c250964b7250d59ea8f29
AdrienGuichard <guichard.adrien@gmail.com>
2020-03-26 07:50:37 +0100

    Add vertical line to deriv Approximation

--- rstudio/approxDerivNlsLM.r
+++ rstudio/approxDerivNlsLM.r
@@ -38,10 +38,25 @@ m <- merge(dfSimu, dfData, all.x=T, by=c(1))
 m$simuDates <- as.Date(m$simuDates, origin = startDate)
 
 #g <- ggplot(m, aes(x=simuDates))+geom_line(aes(y=simu))+geom_line(aes(y=simuMin, color = 'red'))+geom_line(aes(y=simuMax, color = 'blue'))
+#g <- ggplot(m, aes(x=simuDates))+geom_point(aes(y=diffData, color='Real Data'), col="steelblue", size=2)+
+#  geom_line(aes(y=simu))+
+#  geom_line(aes(y=simuMin, color = 'Simu. Min.'))+
+#  geom_line(aes(y=simuMax, color = 'Simu. Max.'))+scale_x_date(date_minor_breaks = "1 day", breaks = "10 day")
+
+
 g <- ggplot(m, aes(x=simuDates))+geom_point(aes(y=diffData, color='Real Data'), col="steelblue", size=2)+
   geom_line(aes(y=simu))+
   geom_line(aes(y=simuMin, color = 'Simu. Min.'))+
-  geom_line(aes(y=simuMax, color = 'Simu. Max.'))+scale_x_date(date_minor_breaks = "1 day", breaks = "10 day")
+  geom_line(aes(y=simuMax, color = 'Simu. Max.'))+
+  scale_x_date(date_minor_breaks = "1 day", breaks = "7 day",
+               labels = scales::date_format("%m-%d"),
+               sec.axis = sec_axis(trans = ~ ., 
+                                   labels = scales::date_format("%m-%d")))+
+  geom_vline(xintercept = as.Date(startDate)+peak, colour = "darkGrey") +
+  geom_text(aes(x=as.Date(startDate)+peak, label="\nDecrease", y=1000), colour="blue", angle=90, text=element_text(size=10)) +
+  geom_text(aes(x=as.Date(startDate)+peak, label="Increase\n", y=1000), colour="red", angle=90, text=element_text(size=10))
+
+
 
 print(g)
 
